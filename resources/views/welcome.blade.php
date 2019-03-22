96fe094d8787a19aa15bac4eda36e8ef330a9a84
Richard A. Hoyle <rhoyle@comcast.com>
2019-03-21 19:39:45 -0500

    nodejs is installed and working just fine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>IronManRecycling</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Iron Man Recycling Jobs
                 </div>
 
                 <div class="links">
